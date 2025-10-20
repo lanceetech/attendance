416d98c0c49c5658b37f3fe885024a3951b04458
dennis Kote <developer.kotez@gmail.com>
2025-10-20 21:29:51 +0000

    the sidebar and the contents of the page seem to  overlapp can you fix t

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -322,7 +322,9 @@ const SidebarInset = React.forwardRef<
     <main
       ref={ref}
       className={cn(
-        "relative flex min-h-svh flex-1 flex-col bg-background",
+        "relative flex min-h-svh flex-1 flex-col bg-background transition-[margin-left] duration-200 ease-linear",
+        "md:peer-data-[state=expanded]:ml-[var(--sidebar-width)]",
+        "md:peer-data-[collapsible=icon]:peer-data-[state=collapsed]:ml-[var(--sidebar-width-icon)]",
         "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         className
       )}
@@ -582,7 +584,7 @@ const SidebarMenuButton = React.forwardRef<
       }
     }
 
-    if (!mounted) {
+    if (!mounted || isMobile === undefined) {
       return button
     }
 

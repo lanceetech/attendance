d05bce6c3ce3309093f85c32044c3533390d6a5e
dennis Kote <developer.kotez@gmail.com>
2025-10-26 20:55:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5825420810-1828c",
+  "appId": "1:681437948087:web:3cce484a647e6ce91e4532",
+  "apiKey": "AIzaSyCaiqhuj2XnjuBWZVHiL3WSaqgtPTAPA_I",
+  "authDomain": "studio-5825420810-1828c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "681437948087"
+};
